5a64d4c92f314782917ea2ce6539c5e889c201e4
Samiul Arafah Dhrubo <66543604+Dhrubo-Arafah@users.noreply.github.com>
2021-08-28 11:30:21 +0600

    Bug fixes

--- script.js
+++ script.js
@@ -3,24 +3,21 @@ const filter = document.getElementById('filter');
 const display = document.getElementById('display');
 const button = document.getElementById('button');
 let text;
+let newArray = [];
 button.addEventListener('click', () => {
+    newArray.length = 0;
     text = phrase.value.trim();
-    display.innerHTML='';
-    reverser(text);
-
-})
-let newArray=[];
-function reverser(text) {
     const array = text.split(" ");
-    for (let index = array.length-1; index >= 0; index--) {
+    for (let index = array.length - 1; index >= 0; index--) {
         newArray.push(array[index])
     }
     print(newArray)
-}
+})
 
-function print(newArray){
-    newArray.map((ele, idx)=>{
-        display.innerHTML+=`<p 
+function print(newArray) {
+    display.innerHTML="";
+    newArray.map(ele => {
+        display.innerHTML += `<p 
         style="
         color:red; 
         margin-right:5px;
@@ -28,16 +25,16 @@ function print(newArray){
         background: #ffd9d9;
         border: 1px dotted red
         ">${ele}</p>`;
-        console.log(idx)
     })
+
 }
 
-filter.addEventListener('keyup',e=>{
-test=filter.value.trim();
-if(newArray.indexOf(test)!==-1){
-    newArray.splice(newArray.indexOf(test), 1);
-}
-display.innerHTML='';
-print(newArray)
-e.preventDefault()
+filter.addEventListener('keyup', e => {
+    test = filter.value.trim();
+    if (newArray.indexOf(test) !== -1) {
+        newArray.splice(newArray.indexOf(test), 1);
+    }
+    display.innerHTML = '';
+    print(newArray)
+    e.preventDefault()
 })
